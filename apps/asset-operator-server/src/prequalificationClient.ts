2e4581bbc89204ff0e6b552a1b50f720fa268c11
John Henderson <john.henderson@energyweb.org>
2021-03-19 14:29:16 -0700

    use convict for config

--- apps/asset-operator-server/src/prequalificationClient.ts
+++ apps/asset-operator-server/src/prequalificationClient.ts
@@ -8,10 +8,9 @@ import { keyManager } from './keyManager';
 export const initPrequalificationClient = (): void => {
   const iamClientParams: IamClientLibFactoryParams = {
     cacheServerUrl: config.prequalification.assetClaimsIam.cacheServerUrl,
-    rpcUrl: config.prequalification.rpcUrl,
+    rpcUrl: config.prequalification.providerUrl,
     chainId: config.prequalification.chainId
   };
-
   const prequalificationClient = new PrequalificationClient(iamClientParams);
   prequalificationClient.init({
     signerProvider: keyManager,
